386a84c9d0cd611900931d5676f948683b834933
progr76@gmail.com <progr76@gmail.com>
2018-07-01 12:42:16 +0000

    version 2

--- Source/run-nw2.js
+++ Source/run-nw2.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const os = require('os');
 //global.DATA_PATH=os.homedir()+"\\TERA";
+if(!global.DATA_PATH || global.DATA_PATH==="")
+    global.DATA_PATH="..\\DATA";
 global.CODE_PATH=process.cwd();
 
 
